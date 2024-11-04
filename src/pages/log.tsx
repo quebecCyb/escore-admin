ad13005ccd6e9bcbf4b7f4a89f4fd408ab9177b4
michaelletichevsky <maslo@dlit.dp.ua>
2024-11-04 18:00:21 +0200

    login reg

--- src/pages/log.tsx
+++ src/pages/log.tsx
@@ -59,6 +59,7 @@ export default function Login() {
                     justify-content: center;
                     min-height: 100vh;
                     background-color: #f0f4f8;
+                    color: black;
                 }
 
                 .login-form {
--- src/pages/reg.tsx
+++ src/pages/reg.tsx
@@ -54,6 +54,7 @@ export default function Register() {
                     justify-content: center;
                     min-height: 100vh;
                     background-color: #f0f4f8;
+                    color: black;
                 }
 
                 .register-form {
